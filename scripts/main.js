509473d20ed1fbfc1519c7b5742a4f6c3f7d74d9
lykamac <141980615+lykamac@users.noreply.github.com>
2024-07-02 16:09:45 +0800

    Update files

--- scripts/main.js
+++ scripts/main.js
@@ -3,10 +3,26 @@ $(function() {
     $('.st-download').on("click", function() {
         var ws_num = $(this).data("worksheet");
 
+        downloadFile(ws_num, "student");
+    });
+
+
+    $('.tc-download').on("click", function() {
+        var ws_num = $(this).data("worksheet");
+        downloadFile(ws_num, "teacher");
+    });
+
+    function downloadFile(ws_num, ws_type) {
+
+
+        if(ws_num == "WS12" || ws_num == "WS14") {
+            return false;
+        }
+
         // Create a hidden anchor element
         var a = document.createElement('a');
-        a.href = `/worksheets/student/EDB_Science_${ws_num}_student.docx`;
-        a.download = `EDB_Science_${ws_num}_student.docx`; // Specify the filename to be downloaded
+        a.href = `/worksheets/${ws_type}/EDB_Science_${ws_num}_${ws_type}.docx`;
+        a.download = `EDB_Science_${ws_num}_${ws_type}.docx`; // Specify the filename to be downloaded
         
         // Append the anchor to the body (required for Firefox)
         document.body.appendChild(a);
@@ -16,5 +32,5 @@ $(function() {
         
         // Remove the anchor from the body
         document.body.removeChild(a);
-    });
+    }
 });
